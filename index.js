b167e5dc5a461d41ec2de231931b8bee062dd724
Meeeees <meesvandijk@mijnheem.nl>
2023-07-17 21:10:04 +0200

    initial commit

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+
+  console.log(req.ip, req.ips, req.connection.remoteAddress)
+  res.json({ ip: req.socket.remoteAddress })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
